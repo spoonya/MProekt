f1aa7f1396642e4c1b5d6bf09254562cda64ba6d
nomad8t <serega18t@tut.by>
2019-12-02 17:40:50 +0300

    Adapted

--- assets/main.js
+++ assets/main.js
@@ -1,40 +1,52 @@
-let intro = $('#intro');
-let header = $('#header');
-let introH = intro.innerHeight();
-let nav = $('#nav');
-let scrollPos = $(this).scrollTop();
+$(function () {
+  /* Fixed header */
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH = intro.innerHeight();;
+  let scrollPos = $(this).scrollTop();
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
 
-function checkScroll (scrollPos, introH) {
-  if (scrollPos > introH) {
-    header.addClass('fixed');
-  } else {
-    header.removeClass('fixed');
+  checkScroll(scrollPos, introH);
+  
+  $(window).on("scroll resize", function () {
+      introH = intro.innerHeight();
+      scrollPos = $(this).scrollTop();
+      checkScroll(scrollPos, introH);
+  });
+
+  function checkScroll(scrollPos, introH) {
+      if (scrollPos > introH) {
+          header.addClass("fixed");
+      }
+      else {
+          header.removeClass("fixed");
+      }
   }
-}
-
-checkScroll(scrollPos, introH);
-
-$(window).on('scroll resize', function () {
-  introH = intro.innerHeight();
-  scrollPos = $(this).scrollTop();
-  console.log(checkScroll(scrollPos, introH));
-  console.log(scrollPos);
+  /* Smooth Scroll */
+  /*$("[data-scroll]").on("click", function (event) {
+      event.preventDefault();
+      let elID = $(this).data('scroll');
+      let elOffset = $(elID).offset().top;
+      nav.removeClass("show");
+      $("html, body").animate({
+          scrollTop: elOffset + 1
+      }, 500);
+  });*/
+  /* Nav Toggle */
+  navToggle.on("click", function (event) {
+      event.preventDefault();
+      nav.toggleClass("show");
+  });
+  
+  /*Glide*/
+  const cfgGlide = {
+    type: 'carousel',
+    perView: 1
+  }
+  new Glide('.glide', cfgGlide).mount();
+  
 });
 
-$('[data-scroll]').on('click', function(event) {
-  event.preventDefault();
-  let elId = $(this).data('scroll');
-  let elOffset = $(elId).offset().top;
-  nav.removeClass('show');
 
-  $('html, body').animate( {
-      scrollTop: elOffset + 1
-    }, 500)
-})
 
-/*Glide*/
-const cfgGlide = {
-        type: 'carousel',
-        perView: 1
-    }
-    new Glide('.glide', cfgGlide).mount()
